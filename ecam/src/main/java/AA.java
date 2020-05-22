7ab40305be8c18778014bd4cd1197f0f86988460
LAPTOP-V2THOMAO\Qianjf <qianjunfeng@bolo.me>
2020-05-22 16:04:43 +0800

    hah

--- ecam/src/main/java/AA.java
+++ ecam/src/main/java/AA.java
@@ -2,4 +2,13 @@
  * Created by Qianjf on 2020/5/22.
  */
 public class AA {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }

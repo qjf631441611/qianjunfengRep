65d961d7bdb473990fe3b636b148bcdbbe3b20ba
LAPTOP-V2THOMAO\Qianjf <qianjunfeng@bolo.me>
2020-05-22 16:12:39 +0800

    one

--- ecam/src/main/java/AA.java
+++ ecam/src/main/java/AA.java
@@ -4,11 +4,22 @@
 public class AA {
     private String name;
 
+    private Long id;
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
